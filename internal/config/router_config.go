c0435b5f19840624cc60b01f540fb32ab9fa6532
Mark Gurianov <mark.gurianov@gmail.com>
2024-04-24 09:49:41 +0300

    Clean slate

--- internal/config/router_config.go
+++
@@ -1,8 +0,0 @@
-package config
-
-const (
-	// BasePath Базовый путь для всей API.
-	BasePath = "/api/v1"
-	// CorsMaxAge Максимальное значение, не игнорируемое ни одним из основных браузеров.
-	CorsMaxAge = 300
-)
